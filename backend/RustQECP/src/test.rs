6a91bd4bafb9a0589c1a4d5e2f293857357585e1
Yue Wu <yue.wu@yale.edu>
2020-11-24 19:12:31 +0800

    add save_load

--- backend/RustQECP/src/test.rs
+++ backend/RustQECP/src/test.rs
@@ -1,5 +1,6 @@
 use super::clap;
 use super::util;
+use super::ndarray;
 
 pub fn run_matched_test(matches: &clap::ArgMatches) {
     match matches.subcommand() {
@@ -11,6 +12,16 @@ pub fn run_matched_test(matches: &clap::ArgMatches) {
 }
 
 fn save_load() {
-    println!("save_load ....");
-    util::load("tmp/save_load.bin");
+    let N = 16;
+    let L = 5;
+    let p = 1e-1;
+    // generate some random data
+    let data = ndarray::Array::from_shape_fn((N, L, L), |_| false);
+    for i in 0..N {
+        println!("i = {}", i);
+    }
+    
+    let (head_r, data_r) =  util::load("tmp/save_load.bin");
+    // println!("{:?}", head_r);
+    // println!("{:?}", data_r);
 }
