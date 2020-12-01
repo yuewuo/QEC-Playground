0cc14ef188980a23706973d1bfc2505143d1b2b5
Yue <wuyue16@pku.edu.cn>
2020-12-01 23:56:14 +0800

    add benchmark script

--- backend/rust/src/tool.rs
+++ backend/rust/src/tool.rs
@@ -29,6 +29,16 @@ pub fn run_matched_tool(matches: &clap::ArgMatches) {
             let qec_decoder = value_t!(matches, "qec_decoder", String).unwrap_or("stupid_decoder".to_string());
             decoder_benchmark(&Ls, &ps, &directory, &qec_decoder);
         }
+        ("automatic_benchmark", Some(matches)) => {
+            let Ls = value_t!(matches, "Ls", String).expect("required");
+            let Ls: Vec<usize> = serde_json::from_str(&Ls).expect("Ls should be [L1,L2,L3,...,Ln]");
+            let ps = value_t!(matches, "ps", String).expect("required");
+            let ps: Vec<f64> = serde_json::from_str(&ps).expect("ps should be [p1,p2,p3,...,pm]");
+            let max_N = value_t!(matches, "max_N", usize).unwrap_or(100000000);  // default to 1e8
+            let min_error_cases = value_t!(matches, "min_error_cases", usize).unwrap_or(1000);  // default to 1e3
+            let qec_decoder = value_t!(matches, "qec_decoder", String).unwrap_or("stupid_decoder".to_string());
+            automatic_benchmark(&Ls, &ps, max_N, min_error_cases, &qec_decoder);
+        }
         _ => unreachable!()
     }
 }
@@ -119,5 +129,50 @@ fn decoder_benchmark(Ls: &Vec<usize>, ps: &Vec<f64>, directory: &str, qec_decode
     }
     if qec_decoder == "stupid_decoder" {
 
+    }
+}
+
+/**
+default example:
+`cargo run --release -- tool automatic_benchmark [3] [3e-2,1e-2,3e-3] -q stupid_decoder`
+**/
+fn automatic_benchmark(Ls: &Vec<usize>, ps: &Vec<f64>, max_N: usize, min_error_cases: usize, qec_decoder: &str) {
+    println!("format: <p> <L> <total_rounds> <qec_failed> <error_rate>");
+    if qec_decoder == "stupid_decoder" {
+        for L in Ls {
+            for p in ps {
+                let p = *p;
+                let L = *L;
+                let no_error = ZxError::new_L(L);
+                let mut x_error_ro = ZxError::new_L(L);
+                let mut rng = thread_rng();
+                let mut total_rounds = 0;
+                let mut qec_failed = 0;
+                while total_rounds < max_N && qec_failed < min_error_cases {
+                    let mut x_error = x_error_ro.view_mut();
+                    let mut has_error = false;
+                    for i in 0..L {
+                        for j in 0..L {
+                            let is_error = rng.gen::<f64>() < p;
+                            x_error[[i, j]] = is_error;
+                            if is_error {
+                                has_error = true;
+                            }
+                        }
+                    }
+                    total_rounds += 1;  // record the total round
+                    if !has_error {
+                        continue
+                    }
+                    let measurement = util::generate_perfect_measurements(&x_error_ro, &no_error);
+                    let (x_correction, _z_correction) = qec::stupid_correction(&measurement);
+                    if x_error_ro.validate_x_correction(&x_correction).is_err() {
+                        qec_failed += 1;
+                    }
+                }
+                let error_rate = qec_failed as f64 / total_rounds as f64;
+                println!("{} {} {} {} {}", p, L, total_rounds, qec_failed, error_rate);
+            }
+        }
     }
 }
---
+++ benchmark/stupid_decoder/stupid_decoder.gp
@@ -0,0 +1,50 @@
+set terminal postscript eps color "Arial, 28"
+set xlabel "Depolarizing probability (p)" font "Arial, 28"
+set ylabel "Logical X error rate (p_L)" font "Arial, 28"
+set grid ytics
+set size 1,1
+
+# data generating commands:
+# cargo run --release -- tool automatic_benchmark [3] [5e-1,2e-1,1e-1,5e-2,2e-2,1e-2,5e-3,2e-3,1e-3,5e-4,2e-4,1e-4,5e-5] -q stupid_decoder
+# cargo run --release -- tool automatic_benchmark [5] [5e-1,2e-1,1e-1,5e-2,2e-2,1e-2,5e-3,2e-3,1e-3,5e-4,2e-4] -q stupid_decoder
+# cargo run --release -- tool automatic_benchmark [7] [5e-1,2e-1,1e-1,5e-2,2e-2,1e-2,5e-3,2e-3,1e-3,5e-4] -q stupid_decoder
+# cargo run --release -- tool automatic_benchmark [9] [5e-1,2e-1,1e-1,5e-2,2e-2,1e-2,5e-3,2e-3,1e-3,5e-4] -q stupid_decoder
+# cargo run --release -- tool automatic_benchmark [11] [5e-1,2e-1,1e-1,5e-2,2e-2,1e-2,5e-3,2e-3,1e-3,5e-4] -q stupid_decoder
+# cargo run --release -- tool automatic_benchmark [13] [5e-1,2e-1,1e-1,5e-2,2e-2,1e-2,5e-3,2e-3] -q stupid_decoder
+# cargo run --release -- tool automatic_benchmark [15] [5e-1,2e-1,1e-1,5e-2,2e-2,1e-2,5e-3,2e-3] -q stupid_decoder
+# cargo run --release -- tool automatic_benchmark [25] [5e-1,2e-1,1e-1,5e-2,2e-2,1e-2,5e-3,2e-3] -q stupid_decoder -m 1000000
+
+set logscale x
+set xrange [0.00001:0.5]
+set xtics ("10^{-5}" 0.00001, "10^{-4}" 0.0001, "10^{-3}" 0.001, "10^{-2}" 0.01, "10^{-1}" 0.1)
+set logscale y
+set ytics ("10^{-8}" 0.00000001, "10^{-7}" 0.0000001, "10^{-6}" 0.000001, "10^{-5}" 0.00001, "10^{-4}" 0.0001, "10^{-3}" 0.001, "10^{-2}" 0.01, "10^{-1}" 0.1)
+set yrange [0.00000001:1]
+set key outside horizontal top center font "Arial, 24"
+
+set style fill transparent solid 0.2 noborder
+
+set output "stupid_decoder.eps"
+
+plot "d_3.txt" using 1:5 with linespoints lt rgb "red" linewidth 5 pointtype 6 pointsize 1.5 title "d = 3",\
+    "d_5.txt" using 1:5 with linespoints lt rgb "blue" linewidth 5 pointtype 2 pointsize 1.5 title "d = 5",\
+    "d_7.txt" using 1:5 with linespoints lt rgb "green" linewidth 5 pointtype 2 pointsize 1.5 title "d = 7",\
+    "d_9.txt" using 1:5 with linespoints lt rgb "yellow" linewidth 5 pointtype 2 pointsize 1.5 title "d = 9",\
+    "d_11.txt" using 1:5 with linespoints lt rgb "purple" linewidth 5 pointtype 2 pointsize 1.5 title "d = 11",\
+    "d_15.txt" using 1:5 with linespoints lt rgb "orange" linewidth 5 pointtype 2 pointsize 1.5 title "d = 15",\
+    "d_25.txt" using 1:5 with linespoints lt rgb "black" linewidth 5 pointtype 2 pointsize 1.5 title "d = 25"
+
+set output '|ps2pdf -dEPSCrop stupid_decoder.eps stupid_decoder.pdf'
+replot
+
+set size 1,0.75
+set output "stupid_decoder_w.eps"
+replot
+set output '|ps2pdf -dEPSCrop stupid_decoder_w.eps stupid_decoder_w.pdf'
+replot
+
+set size 1,0.6
+set output "stupid_decoder_w_w.eps"
+replot
+set output '|ps2pdf -dEPSCrop stupid_decoder_w_w.eps stupid_decoder_w_w.pdf'
+replot
