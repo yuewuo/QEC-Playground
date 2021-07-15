7778b5a84a01e1934e1d7a03809e61b9efa8e504
Yue <yue.wu@yale.edu>
2021-07-15 22:23:00 +0800

    optimize performance

--- benchmark/fault_tolerant_MWPM/automated_threshold_evaluation.py
+++ benchmark/fault_tolerant_MWPM/automated_threshold_evaluation.py
@@ -109,13 +109,17 @@ class AutomatedThresholdEvaluator:
         while upper_bound / lower_bound - 1 > self.target_threshold_accuracy:
             searching_p = math.sqrt(upper_bound * lower_bound)
             error_rate_1, confidence_interval_1, _ = self.simulator_runner(searching_p, self.pair[0], self.parameters, True, self.verbose)
-            error_rate_2, confidence_interval_2, _ = self.simulator_runner(searching_p, self.pair[1], self.parameters, True, self.verbose)
-            if self.verbose:
-                print(f"[{lower_bound}, {upper_bound}] searching_p = {searching_p} [1] {error_rate_1}({confidence_interval_1}) [2] {error_rate_2}({confidence_interval_2})")
             # do not believe the data if logical error rate is too high
-            if error_rate_1 >= self.do_not_believe_logical_error_rate_above or error_rate_2 >= self.do_not_believe_logical_error_rate_above:
+            if error_rate_1 >= self.do_not_believe_logical_error_rate_above:
                 upper_bound = searching_p
                 continue
+            error_rate_2, confidence_interval_2, _ = self.simulator_runner(searching_p, self.pair[1], self.parameters, True, self.verbose)
+            # do not believe the data if logical error rate is too high
+            if error_rate_2 >= self.do_not_believe_logical_error_rate_above:
+                upper_bound = searching_p
+                continue
+            if self.verbose:
+                print(f"[{lower_bound}, {upper_bound}] searching_p = {searching_p} [1] {error_rate_1}({confidence_interval_1}) [2] {error_rate_2}({confidence_interval_2})")
             # # early break if the error rate is already indistinguishable
             # if abs(error_rate_1 - error_rate_2) <= error_rate_1 * confidence_interval_1 + error_rate_2 * confidence_interval_2:
             #     break
