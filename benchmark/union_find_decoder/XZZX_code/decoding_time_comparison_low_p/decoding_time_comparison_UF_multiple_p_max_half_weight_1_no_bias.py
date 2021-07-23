8b3d5cb4a7204275a320567d2597062ac3f0113a
Yue <yue.wu@yale.edu>
2021-07-23 14:02:42 +0800

    add data

--- benchmark/union_find_decoder/XZZX_code/decoding_time_comparison_low_p/decoding_time_comparison_UF_multiple_p_max_half_weight_1_no_bias.py
+++ benchmark/union_find_decoder/XZZX_code/decoding_time_comparison_low_p/decoding_time_comparison_UF_multiple_p_max_half_weight_1_no_bias.py
@@ -7,7 +7,7 @@ from automated_threshold_evaluation import qec_playground_fault_tolerant_MWPM_si
 from automated_threshold_evaluation import run_qec_playground_command_get_stdout
 
 pairs = [ (4, 12, 12), (5, 15, 15), (6, 18, 18), (7, 21, 21), (8, 24, 24) ]  # (di, dj, T)
-p_vec = [0.01 * (10 ** (- i / 3)) for i in range(12)]
+p_vec = [0.01 * (10 ** (- i / 2)) for i in range(6)]
 
 di_vec = [e[0] for e in pairs]
 dj_vec = [e[1] for e in pairs]
@@ -22,8 +22,8 @@ ENABLE_MULTITHREADING = True
 num_threads = os.cpu_count() - 3 if ENABLE_MULTITHREADING else 1
 print(num_threads)
 
-UF_parameters = f"-b10 -p{num_threads} --use_xzzx_code --error_model GenericBiasedWithBiasedCX --bias_eta 1 --decoder UF --max_half_weight 1".split(" ")
-UF_command = qec_playground_fault_tolerant_MWPM_simulator_runner_vec_command(p_vec, di_vec, dj_vec, T_vec, UF_parameters + ["--log_runtime_statistics", "target/decoding_time_UF_multiple_p_max_half_weight_1_no_bias.txt"], max_N=max_N, min_error_cases=min_error_cases)
+UF_parameters = f"-b10 -p{num_threads} --use_xzzx_code --error_model GenericBiasedWithBiasedCX --bias_eta 100 --decoder UF --max_half_weight 1".split(" ")
+UF_command = qec_playground_fault_tolerant_MWPM_simulator_runner_vec_command(p_vec, di_vec, dj_vec, T_vec, UF_parameters + ["--log_runtime_statistics", "target/decoding_time_UF_multiple_p_max_half_weight_1.txt"], max_N=max_N, min_error_cases=min_error_cases)
 print(" ".join(UF_command))
 
 # UF
