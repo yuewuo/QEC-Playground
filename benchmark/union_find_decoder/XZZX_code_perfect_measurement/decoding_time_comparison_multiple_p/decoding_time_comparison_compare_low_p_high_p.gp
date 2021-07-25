20cc72a1d5761ac9e2160365c0288fc45b52425b
Yue <yue.wu@yale.edu>
2021-07-25 12:10:11 +0800

    add data

---
+++ benchmark/union_find_decoder/XZZX_code_perfect_measurement/decoding_time_comparison_multiple_p/decoding_time_comparison_compare_low_p_high_p.gp
@@ -0,0 +1,43 @@
+set terminal postscript eps color "Arial, 28"
+set xlabel "Code Distance (d)" font "Arial, 28"
+set ylabel "Avergae Decoding Time (s)" font "Arial, 28"
+# set grid ytics
+set size 1,1
+
+set logscale x
+set xrange [5:63]
+# labels
+# python -c "for i in [5, 7, 9, 11, 13, 17, 21, 25, 29, 37, 43, 53, 63][::2]: print('\'%d\' %d' % (i, i), end=', ')"
+set xtics ('5' 5, '9' 9, '13' 13, '21' 21, '29' 29, '43' 43, '63' 63)
+set logscale y
+set yrange [0.00001:0.1]
+# labels
+# python -c "for i in range(0, 5): print('\'%.0e\' %.0e' % tuple([0.00001 * (10**i) for j in range(2)]), end=', ')"
+set ytics ('1e-05' 1e-05, '1e-04' 1e-04, '1e-03' 1e-03, '1e-02' 1e-02, '1e-01' 1e-01)
+set key outside horizontal top center font "Arial, 24"
+
+set style fill transparent solid 0.2 noborder
+
+set title "Decoding Time of XZZX Code d x d x 0"
+
+set output "decoding_time_comparison_compare_low_p_high_p.eps"
+
+plot "decoding_time_p_0.01.txt" using 1:2 with linespoints lt rgb "#e41a1c" linewidth 3 pointtype 7 pointsize 1 title "p = 0.01",\
+    "decoding_time_p_0.03.txt" using 1:2 with linespoints lt rgb "#377eb8" linewidth 3 pointtype 9 pointsize 1 title "p = 0.03",\
+    "decoding_time_p_0.1.txt" using 1:2 with linespoints lt rgb "#4daf4a" linewidth 3 pointtype 11 pointsize 1 title "p = 0.1",\
+    "decoding_time_p_0.3.txt" using 1:2 with linespoints lt rgb "#ff7f00" linewidth 3 pointtype 5 pointsize 1 title "p = 0.3"
+
+set output '|ps2pdf -dEPSCrop decoding_time_comparison_compare_low_p_high_p.eps decoding_time_comparison_compare_low_p_high_p.pdf'
+replot
+
+set size 1,0.75
+set output "decoding_time_comparison_compare_low_p_high_p_w.eps"
+replot
+set output '|ps2pdf -dEPSCrop decoding_time_comparison_compare_low_p_high_p_w.eps decoding_time_comparison_compare_low_p_high_p_w.pdf'
+replot
+
+set size 1,0.6
+set output "decoding_time_comparison_compare_low_p_high_p_w_w.eps"
+replot
+set output '|ps2pdf -dEPSCrop decoding_time_comparison_compare_low_p_high_p_w_w.eps decoding_time_comparison_compare_low_p_high_p_w_w.pdf'
+replot
---
+++ benchmark/union_find_decoder/XZZX_code_perfect_measurement/decoding_time_comparison_multiple_p/process_data_compare_low_p_high_p.py
@@ -0,0 +1,83 @@
+import sys, os, json, math
+import scipy.stats
+
+fixed_configuration = None
+configurations = []
+data_vec = []
+
+with open("pm_decoding_time_compare_low_p_high_p.txt", "r", encoding="utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip(" \r\n")
+        if line == "":  # ignore empty line
+            continue
+        if line[:3] == "#f ":
+            fixed_configuration = json.loads(line[3:])
+        elif line[:2] == "# ":
+            configurations.append(json.loads(line[2:]))
+            data_vec.append([])
+        else:
+            data_vec[-1].append(json.loads(line))
+
+print(fixed_configuration)
+
+def average(lst):
+    return sum(lst) / len(lst)
+
+p_vec = [0.01, 0.03, 0.1, 0.3]
+fitting_data_vec = [[] for di in range(len(p_vec))]
+
+for i in range(0, len(configurations)):
+    config = configurations[i]
+    vec = data_vec[i]
+    idx = -1
+    for i in range(len(p_vec)):
+        p = p_vec[i]
+        ratio = config["p"] / p
+        if ratio > 0.99 and ratio < 1.01:
+            idx = i
+    assert idx >= 0, "must find similar p"
+    fitting_data = fitting_data_vec[idx]
+    error_count = 0
+    success_count = 0
+    # these only accounts successful cases
+    time_build_decoders_vec = []
+    time_run_to_stable_vec = []
+    time_build_decoders_run_to_stable_vec = []
+    for e in vec:
+        if e["error"]:
+            error_count += 1
+        else:
+            success_count += 1
+            time_build_decoders_vec.append(e["time_build_decoders"])
+            time_run_to_stable_vec.append(e["time_run_to_stable"])
+            time_build_decoders_run_to_stable_vec.append(e["time_build_decoders"] + e["time_run_to_stable"])
+    upper_idx = min(max(0, int(success_count - error_count * 0.1)), success_count - 1)  # this will lead to error rate of 110% x original error rate
+    print(f"error: {error_count}, success_count: {success_count}, error_rate: {error_count/(error_count+success_count)}")
+    print(f"time_build_decoders: {average(time_build_decoders_vec)}, {sorted(time_build_decoders_vec)[upper_idx]}")
+    print(f"time_run_to_stable: {average(time_run_to_stable_vec)}, {sorted(time_run_to_stable_vec)[upper_idx]}")
+    print(f"time_build_decoders_run_to_stable: {average(time_build_decoders_run_to_stable_vec)}, {sorted(time_build_decoders_run_to_stable_vec)[upper_idx]}")
+    fitting_data.append((config["di"], average(time_run_to_stable_vec)))
+
+for i in range(len(p_vec)):
+    p = p_vec[i]
+    fitting_data = fitting_data_vec[i]
+    X = [math.log(e[0]) for e in fitting_data]
+    Y = [math.log(e[1]) for e in fitting_data]
+    slope, intercept, r, _, _ = scipy.stats.linregress(X, Y)
+    print("\n\n")
+    print(f"p = {p}")
+    print(fitting_data)
+    print(f"slope = {slope}")
+    print(f"intercept = {intercept}")
+    print(f"r_square = {r**2}")
+    for e in fitting_data:
+        print(f"{e[0]} {e[1]}")
+
+for i in range(len(p_vec)):
+    p = p_vec[i]
+    fitting_data = fitting_data_vec[i]
+    X = [math.log(e[0]) for e in fitting_data]
+    Y = [math.log(e[1]) for e in fitting_data]
+    slope, intercept, r, _, _ = scipy.stats.linregress(X, Y)
+    print(f"{p} {slope} {r**2}")
