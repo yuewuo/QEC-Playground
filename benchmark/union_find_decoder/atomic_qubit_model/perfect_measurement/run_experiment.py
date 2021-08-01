a7ae1926a9e29b12159730eb4cb44f878ccc7f6d
Yue <yue.wu@yale.edu>
2021-08-01 20:51:53 +0800

    Update run_experiment.py

--- benchmark/union_find_decoder/atomic_qubit_model/perfect_measurement/run_experiment.py
+++ benchmark/union_find_decoder/atomic_qubit_model/perfect_measurement/run_experiment.py
@@ -37,7 +37,7 @@ for di in di_vec:
         batch = 1000
         if di == 3:
             batch = 10000
-        UF_command = qec_playground_fault_tolerant_MWPM_simulator_runner_vec_command([p_pauli], [di], [di], [0], UF_parameters + ["--log_runtime_statistics", "target/pm_decoding_time_multiple_p.txt", "--pes", f"[{p_erasure}]", f"-p{batch}"], max_N=max_N, min_error_cases=min_error_cases)
+        UF_command = qec_playground_fault_tolerant_MWPM_simulator_runner_vec_command([p_pauli], [di], [di], [0], UF_parameters + ["--log_runtime_statistics", "target/pm_decoding_time_multiple_p.txt", "--pes", f"[{p_erasure}]", f"-b{batch}"], max_N=max_N, min_error_cases=min_error_cases)
         print(" ".join(UF_command))
 
         # run experiment
