5186f31a9572803a5718d075cfb645ba47be9daa
Yue <yue.wu@yale.edu>
2021-09-09 11:36:42 -0400

    plot

--- benchmark/union_find_decoder/atomic_qubit_model/revised_95_5_mixed_gate_only_circuit_level/study_error_pattern_causing_logical_error/process_data.py
+++ benchmark/union_find_decoder/atomic_qubit_model/revised_95_5_mixed_gate_only_circuit_level/study_error_pattern_causing_logical_error/process_data.py
@@ -1,10 +1,13 @@
 import json
 
 di_vec = [3, 5, 7, 9]
+quantization = 50
+
 for di in di_vec:
     log_filename = f"study_error_pattern_causing_logical_error_d_{di}.txt"
     with open(log_filename, "r", encoding="utf8") as f:
         lines = f.readlines()
+    quantized_erasure_acc = [0 for i in range(quantization)]
     for line in lines:
         line = line.strip("\r\n ")
         if line[0] == "#":
@@ -12,5 +15,22 @@ for di in di_vec:
         data = json.loads(line)
         if data["error"] is False:
             continue  # don't care cases without logical error
-        print(data)
-    exit(0)
+        has_erasure_count = 0
+        no_erasure_count = 0
+        for element in data["error_pattern"]:
+            t, i, j, error, has_erasure = element
+            if has_erasure:
+                has_erasure_count += 1
+            else:
+                no_erasure_count += 1
+        erasure_ratio = has_erasure_count / (has_erasure_count + no_erasure_count)
+        quantized_erasure_ratio = max(0, min(quantization - 1, int(erasure_ratio * quantization)))
+        quantized_erasure_acc[quantized_erasure_ratio] += 1
+    print(f"d = {di}")
+    print(quantized_erasure_acc)
+    overall_size = sum(quantized_erasure_acc)
+    print([e / overall_size for e in quantized_erasure_acc])
+    print("")
+    for i in range(quantization):
+        print((i + 0.5) / quantization, quantized_erasure_acc[i], quantized_erasure_acc[i] / overall_size)
+    print("")
