75c80aa07e48ba2af78b00b5b227e794fbcb6bb1
Yue Wu <yue.wu@yale.edu>
2022-03-03 13:06:07 -0500

    fix build on mac

--- backend/rust/build.rs
+++ backend/rust/build.rs
@@ -1,14 +1,18 @@
 extern crate cc;
+use std::env;
 
 fn main() {
+
+    let target_os = env::var("CARGO_CFG_TARGET_OS");
 
     cc::Build::new()
         .cpp(true)
         .file("../../backend/blossomV/test.cpp")
         .compile("test");
     
-    cc::Build::new()
-        .cpp(true)
+    let mut build = cc::Build::new();
+
+    build.cpp(true)
         .file("../../backend/blossomV/blossomV.cpp")
         .file("../../backend/blossomV/PMinterface.cpp")
         .file("../../backend/blossomV/PMduals.cpp")
@@ -18,16 +22,24 @@ fn main() {
         .file("../../backend/blossomV/PMrepair.cpp")
         .file("../../backend/blossomV/PMshrink.cpp")
         .file("../../backend/blossomV/misc.cpp")
-        .file("../../backend/blossomV/MinCost/MinCost.cpp")
-        .cpp_link_stdlib("stdc++") // use libstdc++
-        .flag("-Wno-unused-parameter")
+        .file("../../backend/blossomV/MinCost/MinCost.cpp");
+
+    if target_os != Ok("macos".to_string()) {  // exclude from macOS
+        build.cpp_link_stdlib("stdc++"); // use libstdc++
+    }
+
+    build.flag("-Wno-unused-parameter")
         .flag("-Wno-unused-variable")
         .flag("-Wno-unused-but-set-variable")
         .compile("blossomV");
 
     println!("cargo:rerun-if-changed=../../backend/blossomV/test.cpp");
     println!("cargo:rerun-if-changed=../../backend/blossomV/blossomV.cpp");
-    println!("cargo:rustc-link-lib=static=stdc++");  // have to add this to compile c++ (new, delete operators)
+
+    if target_os != Ok("macos".to_string()) {  // exclude from macOS
+        println!("cargo:rustc-link-lib=static=stdc++");  // have to add this to compile c++ (new, delete operators)
+    }
+
     println!("cargo:rustc-link-lib=static=test");
     println!("cargo:rustc-link-lib=static=blossomV");
 }
