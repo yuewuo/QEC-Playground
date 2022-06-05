43b2f29831559d8a272470b640f5e54976987db0
m24639297 <m24639297@gmail.com>
2022-06-04 20:55:18 -0400

    begin my first try on bell init

--- backend/rust/src/error_model_builder.rs
+++ backend/rust/src/error_model_builder.rs
@@ -17,6 +17,7 @@ pub enum ErrorModelBuilder {
     Phenomenological,
     /// tailored surface code with Bell state initialization (logical |+> state) to fix 3/4 of all stabilizers
     TailoredScBellInitPhenomenological,
+    TailoredScBellInitCircuit,
     /// arXiv:2104.09539v1 Sec.IV.A
     GenericBiasedWithBiasedCX,
     /// arXiv:2104.09539v1 Sec.IV.A
@@ -119,6 +120,7 @@ impl ErrorModelBuilder {
                 });
             },
             ErrorModelBuilder::GenericBiasedWithBiasedCX | ErrorModelBuilder::GenericBiasedWithStandardCX => {
+                // (here) FIRST qubit: anc; SECOND: data, due to circuit design
                 let mut initialization_error_rate = p;  // by default initialization error rate is the same as p
                 let mut measurement_error_rate = p;
                 let mut config_cloned = error_model_configuration.clone();
@@ -201,6 +203,53 @@ impl ErrorModelBuilder {
                     }
                 });
             },
+            ErrorModelBuilder::TailoredScBellInitCircuit => {
+                let (noisy_measurements, dp, dn) = match simulator.code_type {
+                    CodeType::RotatedTailoredCode{ noisy_measurements, dp, dn } => { (noisy_measurements, dp, dn) }
+                    _ => unimplemented!("tailored surface code with Bell state initialization is only implemented for open-boundary rotated tailored surface code")
+                };
+                assert!(noisy_measurements > 0, "to simulate bell initialization, noisy measurement must be set +1 (e.g. set noisy measurement 1 is equivalent to 0 noisy measurements)");
+                assert!(simulator.measurement_cycles > 1);
+                // change all stabilizers at the first round as virtual
+                simulator_iter_mut!(simulator, position, node, t => simulator.measurement_cycles, {
+                    if node.qubit_type != QubitType::Data {
+                        assert!(node.gate_type.is_measurement());
+                        assert!(node.gate_type.is_single_qubit_gate());
+                        // since no peer, just set myself as virtual is ok
+                        node.is_virtual = true;
+                        error_model.set_node(position, Some(noiseless_node.clone()));  // clear existing noise model
+                    }
+                });
+                let simulator = &*simulator;  // force simulator to be immutable, to avoid unexpected changes
+                assert!(px + py + pz <= 1. && px >= 0. && py >= 0. && pz >= 0.);
+                assert!(pe == 0.);  // phenomenological error model doesn't support erasure errors
+                if simulator.measurement_cycles == 1 {
+                    eprintln!("[warning] setting error rates of unknown code, no perfect measurement protection is enabled");
+                }
+                // create an error model that is always 50% change of measurement error
+                let mut messed_measurement_node = ErrorModelNode::new();
+                messed_measurement_node.pauli_error_rates.error_rate_Y = 0.5;  // Y error will cause pure measurement error for StabX (X basis), StabZ (Z basis), StabY (X basis)
+                let messed_measurement_node = Arc::new(messed_measurement_node);
+                simulator_iter_real!(simulator, position, node, {
+                    error_model.set_node(position, Some(noiseless_node.clone()));  // clear existing noise model
+                    if position.t == simulator.measurement_cycles - 1 {
+                        for i in 0..((dn+1)/2-1) {
+                            for j in 0..(dp+1)/2 {
+                                error_model.set_node(&pos!(position.t, 3 + 2*i + 2*j, dn-1 - 2*i + 2*j), Some(messed_measurement_node.clone()));
+                            }
+                        }
+                    } else if position.t >= simulator.measurement_cycles {  // no error before the first round
+                        if position.t < simulator.height - simulator.measurement_cycles {  // no error at the final perfect measurement round
+                            if position.t % simulator.measurement_cycles == 0 && node.qubit_type == QubitType::Data {
+                                error_model.set_node(position, Some(biased_node.clone()));
+                            }
+                            if (position.t + 1) % simulator.measurement_cycles == 0 && node.qubit_type != QubitType::Data {  // measurement error must happen before measurement round
+                                error_model.set_node(position, Some(pure_measurement_node.clone()));
+                            }
+                        }
+                    }
+                });
+            }
         }
     }
 }
---
+++ benchmark/tailored_mwpm_decoder/bell_init_pheno_thres/evaluate_threshold.py
@@ -0,0 +1,134 @@
+import os, sys, subprocess, hjson, datetime
+qec_playground_root_dir = subprocess.run("git rev-parse --show-toplevel", cwd=os.path.dirname(os.path.abspath(__file__)), shell=True, check=True, capture_output=True).stdout.decode(sys.stdout.encoding).strip(" \r\n")
+rust_dir = os.path.join(qec_playground_root_dir, "backend", "rust")
+sys.path.insert(0, os.path.join(qec_playground_root_dir, "benchmark", "slurm_utilities"))
+import slurm_distribute
+from slurm_distribute import slurm_threads_or as STO
+sys.path.insert(0, os.path.join(qec_playground_root_dir, "benchmark", "threshold_analyzer"))
+from threshold_analyzer import qecp_benchmark_simulate_func_command_vec
+from threshold_analyzer import run_qecp_command_get_stdout, compile_code_if_necessary
+from threshold_analyzer import ThresholdAnalyzer
+
+def rough_code_distances(bias_eta):
+    return [4, 6] if bias_eta < 1000 else [6, 8] # larger code distance is necessary for high code distance
+def rough_runtime_budgets(bias_eta):
+    return [(6000, 600), (6000, 2400)] if bias_eta < 1000 else [(6000, 3600), (6000, 3600)]
+rough_init_search_start_p = 0.1  # already know all possible threshold is below 15%
+code_distances = [6, 8, 10, 12]
+runtime_budgets = [(180000, 3600 * 4)] * len(code_distances)  # each given one hour
+bias_eta_vec = [10, 100] #[10, 30, 100, 300, 1000, 1e200]  # only one bias
+
+slurm_distribute.SLURM_DISTRIBUTE_TIME = "4:20:00"
+slurm_distribute.SLURM_DISTRIBUTE_MEM_PER_TASK = '16G'
+slurm_distribute.SLURM_DISTRIBUTE_CPUS_PER_TASK = 12  # for more usuable machines, use `SLURM_USE_SCAVENGE_PARTITION=1` flag
+def generate_parameters(bias_eta):
+    return f"-p{STO(0)} --code_type RotatedTailoredCode --bias_eta {bias_eta} --decoder tailored-mwpm --decoder_config {{\"pcmg\":true}} --error_model tailored-sc-bell-init-phenomenological".split(" ")
+
+PRECISE_RESULT_FILE = os.path.join(os.path.dirname(__file__), f"precise_result.hjson")
+RESULT_FILE = os.path.join(os.path.dirname(__file__), f"result.hjson")
+HISTORY_FILE = os.path.join(os.path.dirname(__file__), f"history.tmp")  # to avoid deleting important result
+def time_str():
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M:%S.%f")\
+
+for result_file in [RESULT_FILE, PRECISE_RESULT_FILE]:
+    if not os.path.exists(result_file):
+        with open(result_file, "w", encoding="utf8") as f:
+            f.write(hjson.dumps({}, sort_keys=True))
+            f.flush()
+
+def truncate_write_file(f, content):
+    f.seek(0)
+    f.truncate()
+    f.write(content)
+    f.flush()
+
+# once a rough estimate is missing, this will be set to True, and no precise estimation will be run afterwards
+ROUGH_ESTIMATE_ONLY = False
+
+compile_code_if_necessary()
+@slurm_distribute.slurm_distribute_run(os.path.dirname(__file__))
+def experiment(slurm_commands_vec = None, run_command_get_stdout=run_qecp_command_get_stdout):
+
+    for bias_eta in bias_eta_vec:
+        with open(RESULT_FILE, "r", encoding="utf8") as f:
+            log_obj = hjson.loads(f.read())
+            need_rough_estimate = f"bias_eta_{bias_eta}" not in log_obj
+        if need_rough_estimate:
+            global ROUGH_ESTIMATE_ONLY
+            ROUGH_ESTIMATE_ONLY = True
+
+    for bias_eta in bias_eta_vec:
+        print(f"bias_eta: {bias_eta}")
+
+        parameters = generate_parameters(bias_eta)
+        def generate_command(p, d, runtime_budget, p_center=None):
+            min_error_case, time_budget = runtime_budget
+            command = qecp_benchmark_simulate_func_command_vec(p, d, d, d, parameters, min_error_cases=min_error_case, time_budget=time_budget, p_graph=p_center)
+            return command
+
+        def simulate_func(p, d, runtime_budget, p_graph=None):
+            command = generate_command(p, d, runtime_budget, p_graph)
+            stdout, returncode = run_command_get_stdout(command)
+            assert returncode == 0, "command fails..."
+            full_result = stdout.strip(" \r\n").split("\n")[-1]
+            lst = full_result.split(" ")
+            error_rate = float(lst[5])
+            confidence_interval = float(lst[7])
+            return (error_rate, confidence_interval)
+        threshold_analyzer = ThresholdAnalyzer(code_distances, simulate_func)
+        threshold_analyzer.rough_code_distances = rough_code_distances(bias_eta)
+        threshold_analyzer.verbose = True
+        threshold_analyzer.rough_runtime_budgets = rough_runtime_budgets(bias_eta)
+        threshold_analyzer.rough_init_search_start_p = rough_init_search_start_p
+        threshold_analyzer.code_distances = code_distances
+        threshold_analyzer.runtime_budgets = runtime_budgets
+
+        with open(RESULT_FILE, "r", encoding="utf8") as f:
+            log_obj = hjson.loads(f.read())
+            need_rough_estimate = f"bias_eta_{bias_eta}" not in log_obj
+        if need_rough_estimate:
+            if slurm_commands_vec is not None:
+                print("[warning] you're running rough estimation in slurm cluster")
+                print("          the best practice is to run it locally and synchronize result.hjson to slurm cluster")
+                exit(1)
+            # run rough estimate and save to file
+            rough_popt, rough_perr = threshold_analyzer.rough_estimate()
+            with open(RESULT_FILE, "r+", encoding="utf8") as f:
+                log_obj = hjson.loads(f.read())
+                log_obj[f"bias_eta_{bias_eta}"] = [rough_popt, rough_perr]
+                truncate_write_file(f, hjson.dumps(log_obj, sort_keys=True))
+            with open(HISTORY_FILE, "a", encoding="utf8") as f:
+                f.write(f"{time_str()}\n")
+                f.write(hjson.dumps(["rough estimate", bias_eta, rough_popt, rough_perr]))
+                f.write("\n\n")
+                f.flush()
+        
+        if ROUGH_ESTIMATE_ONLY:
+            continue
+
+        # read existing estimate (not necessarily rough estimate, but can also be a previous precise estimate)
+        with open(RESULT_FILE, "r", encoding="utf8") as f:
+            log_obj = hjson.loads(f.read())
+            rough_popt, rough_perr = log_obj[f"bias_eta_{bias_eta}"]
+        if slurm_commands_vec is not None:
+            collected_parameters, p_list = threshold_analyzer.precise_estimate_parameters(rough_popt)
+            for collected_parameters_row in collected_parameters:
+                for (p, d, runtime_budget, p_center) in collected_parameters_row:
+                    command = generate_command(p, d, runtime_budget, p_center)
+                    slurm_commands_vec.sanity_checked_append(command)
+            continue  # skip handling results
+    
+        # at this point, if in slurm, all results have been cached
+        popt, perr = threshold_analyzer.precise_estimate(rough_popt)
+        with open(PRECISE_RESULT_FILE, "r+", encoding="utf8") as f:
+            log_obj = hjson.loads(f.read())
+            log_obj[f"bias_eta_{bias_eta}"] = [popt, perr]
+            truncate_write_file(f, hjson.dumps(log_obj, sort_keys=True))
+        with open(HISTORY_FILE, "a", encoding="utf8") as f:
+            f.write(f"{time_str()}\n")
+            f.write(hjson.dumps(["precise estimate", bias_eta, popt, perr]))
+            f.write("\n\n")
+            f.flush()
+        image_path = os.path.join(os.path.dirname(__file__), f"threshold_bias_eta_{bias_eta}.pdf")
+        threshold_analyzer.save_image_collected_data(image_path, popt, perr, *threshold_analyzer.collected_data_list[-1])
