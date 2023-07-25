09a592e60b2e0468d8a3336a77bede113026ff60
Yue Wu <yue.wu@yale.edu>
2023-07-24 19:09:40 -0700

    add samples

--- visualize/generate_samples.sh
+++ visualize/generate_samples.sh
@@ -20,3 +20,12 @@ cargo run --release -- tool benchmark '[9]' '[1]' '[0.10]' --bias-eta 1e100 --co
 
 # Tailored SC decoder: code capacity with high bias
 cargo run --release -- tool benchmark '[7]' '[0]' '[0.10]' --bias-eta 1e100 --code-type rotated-tailored-code --decoder tailored-mwpm --decoder-config '{"pcmg":true,"naive_residual_decoding":true,"log_matchings":true}' --ignore-logical-j --enable-visualizer --visualizer-filename tailored-cc-no-init-high-bias.json --visualizer-model-graph --visualizer-tailored-model-graph --visualizer-model-hypergraph --deterministic-seed 123 -m 200
+
+# a group of simple cases
+cargo run --release -- tool benchmark '[3]' '[3]' '[0.01]' --code-type rotated-planar-code --noise-model stim-noise-model --decoder fusion --decoder-config '{"log_matchings":true}' --enable-visualizer --visualizer-filename rotated-surface-code-decoding-graph-d3.json --visualizer-model-graph --visualizer-model-hypergraph --deterministic-seed 123 -m 200
+
+cargo run --release -- tool benchmark '[3]' '[3]' '[0.01]' --code-type standard-planar-code --noise-model stim-noise-model --decoder fusion --decoder-config '{"log_matchings":true}' --enable-visualizer --visualizer-filename rotated-surface-code-decoding-graph-d3.json --visualizer-model-graph --visualizer-model-hypergraph --deterministic-seed 123 -m 200
+
+cargo run --release -- tool benchmark '[5]' '[5]' '[0.01]' --code-type rotated-planar-code --noise-model stim-noise-model --decoder fusion --decoder-config '{"log_matchings":true}' --enable-visualizer --visualizer-filename rotated-surface-code-decoding-graph-d5.json --visualizer-model-graph --visualizer-model-hypergraph --deterministic-seed 123 -m 200
+
+cargo run --release -- tool benchmark '[5]' '[5]' '[0.01]' --code-type standard-planar-code --noise-model stim-noise-model --decoder fusion --decoder-config '{"log_matchings":true}' --enable-visualizer --visualizer-filename rotated-surface-code-decoding-graph-d5.json --visualizer-model-graph --visualizer-model-hypergraph --deterministic-seed 123 -m 200
