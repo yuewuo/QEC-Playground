100b7c22c5ae066ec03222e8e33585e4845774e8
Leo <binghong.li@yale.edu>
2024-11-19 03:00:31 +0000

    comply with mwpf's cloned syndrome pattern and integral updates to negative-weighted edges

--- src/decoder_hyperion.rs
+++ src/decoder_hyperion.rs
@@ -193,7 +193,7 @@ impl HyperionDecoder {
             })
             .collect();
 
-        let mut syndrome_pattern = SyndromePattern::new(defect_vertices, vec![]);
+        let syndrome_pattern = SyndromePattern::new(defect_vertices, vec![]);
 
         let decoder_begin = Instant::now();
 
@@ -212,7 +212,7 @@ impl HyperionDecoder {
 
         let time_decode_bp = decoder_begin.elapsed().as_secs_f64();
 
-        self.solver.solve(&mut syndrome_pattern);
+        self.solver.solve(syndrome_pattern);
         let subgraph = self.solver.subgraph();
         self.solver.clear();
 
